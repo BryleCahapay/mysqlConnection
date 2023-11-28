261d35b8712441847358f0c2d5ef392085bbdce3
Admin <Admin@DESKTOP-104P2PL>
2023-11-28 23:32:34 +0800

    Add project files.

---
+++ myclass/connection_class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace mysqlConnection.myclass
+{
+    internal class connection_class
+    {
+        public MySqlConnection connectvs;
+
+        public connection_class() 
+        {
+            string host = "localhost";
+            string database = "studentvs";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+            string connection_string = "datasource =" + host + ";database=" + database + "; port=" + port + "; username =" + username + ";" +
+                "password=" + password + "; SslMode =none;";
+
+            connectvs = new MySqlConnection(connection_string);
+        }
+    }
+}
